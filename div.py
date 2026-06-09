3cb5948086f5a958c032f6832f2c0d68426f3dcd
Walizzz_ <oualidbi2006@gmail.com>
2026-06-09 18:03:34 +0100

    Add files via upload

---
+++ div.py
@@ -0,0 +1,25 @@
+'''
+Task
+The provided code stub reads two integers, a and , b from STDIN.
+
+Add logic to print two lines. The first line should contain the result of integer division,  // . The second line should contain the result of float division,  a / b.
+
+No rounding or formatting is necessary.
+
+Example
+a = 3
+b = 5
+
+The result of the integer division 3 // 5 = 0.
+The result of the float division is 3 / 5 = 0.6.
+'''
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+    div1 = a // b
+    div2 = a / b
+    
+    print(div1)
+    print(div2)
---
+++ ssss.py
@@ -0,0 +1,5 @@
+n = 3
+for i in range(0, 5):
+    while(i < n):
+        i =  i ** i 
+    print(i)
